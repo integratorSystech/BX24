9103584e6d700ee3d38ba2be1eae55f39695ed73
EpickFail <integrator@systech.ru>
2020-05-28 16:11:01 +0500

    add print error on fyllbatch

--- src/FullBatch.php
+++ src/FullBatch.php
@@ -6,9 +6,19 @@ class FullBatch extends DecoratorBitrix24
     public function batch($fields)
     {          
         while (count($fields) > 0) {
-            print_r(count($fields));
-            echo '<br>';
-            $this->data['result'] = array_merge($this->data['result'], parent::batch(array_splice($fields, 0, 50))['result']);  
+            $res = parent::batch(array_splice($fields, 0, 50));
+            
+            if (!empty($res['result'])) {
+                $this->data['result'] = array_merge(
+                    $this->data['result'],
+                    $res['result']
+                );  
+            } else {
+                $this->data['result'] = array_merge(
+                    $this->data['result'],
+                    $res
+                );  
+            }
         }
         return $this->data;
     }
