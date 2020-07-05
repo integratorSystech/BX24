fcd7d3acb8d3fdb8b014bd5df084fa877643aa5b
integratorSystech <60893979+integratorSystech@users.noreply.github.com>
2020-07-05 23:12:36 +0500

    Update FullBatch.php

--- src/FullBatch.php
+++ src/FullBatch.php
@@ -5,7 +5,7 @@ class FullBatch extends DecoratorBitrix24
 {    
     public function batch($fields)
     {   
-        $data = [];
+        $data['result'] = [];
         while (count($fields) > 0) {
             $res = parent::batch(array_splice($fields, 0, 50));
             
